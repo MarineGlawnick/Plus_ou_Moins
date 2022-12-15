952d19c25761da3a6e1dcaef43cb14f40e57e9ed
Marine Glawnick <mglawnick@seela.io>
2022-12-15 11:46:52 +0100

    add algo random number

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('hello');
+function getRandomNumber(maxNumber) {
+    return Math.floor(Math.random() * (maxNumber + 1));
+}
+var solution = getRandomNumber(100);
+console.log(solution);
--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
-console.log('hello')
+
+function getRandomNumber(maxNumber): number{
+return Math.floor(Math.random() * (maxNumber + 1));
+}
+const solution = getRandomNumber(100);
+console.log(solution)
